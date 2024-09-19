575d301fbe0629b5f652e6b86161c36453dc94b8
phillima <phyllipe_slf@yahoo.com.br>
2024-09-19 19:01:40 -0300

    Aula 19/09 - Animção Idle/Running Player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,23 +4,38 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Utils;
 
 public class PlayerScript : MonoBehaviour
 {
-    
+    Animator _playerAnimator;
     Rigidbody2D _playerRb;
-
     Vector2 horMov;
+
+    [SerializeField]
+    float horSpeed;
     
     void Awake()
     {
         _playerRb = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
     }
-
 
     void MovePlayer()
     {
-        _playerRb.velocity = new Vector2(horMov.x * 5,_playerRb.velocity.y);
+        _playerRb.velocity = new Vector2(horMov.x * horSpeed,_playerRb.velocity.y);
+
+        bool isRunning = Mathf.Abs(_playerRb.velocity.x) > Mathf.Epsilon;
+        _playerAnimator.SetBool(Constantes.ANIM_IS_RUNNING,isRunning);
+        if (isRunning)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        transform.localScale = new Vector3(Mathf.Sign(_playerRb.velocity.x),1,1);
     }
     
     void OnMove(InputValue inputValue)
@@ -30,7 +45,7 @@ public class PlayerScript : MonoBehaviour
 
     void OnJump(InputValue inputValue)
     {
-        print("pulou");
+        _playerRb.velocity = new Vector2(0, 5);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Utils/Constantes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Utils
+{
+    public static class Constantes 
+    {
+        public const string ANIM_IS_RUNNING = "isRunning";
+    }
+}
